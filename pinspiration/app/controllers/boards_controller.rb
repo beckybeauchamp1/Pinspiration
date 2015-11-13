df3ea99aeb315c69fdbca5c41d5245677fdbdafc
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-13 11:29:34 -0500

    nested routes

---
+++ pinspiration/app/controllers/boards_controller.rb
@@ -0,0 +1,12 @@
+class BoardsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    @boards = Board.all
+    
+  end
+
+
+
+end
--- pinspiration/config/routes.rb
+++ pinspiration/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'pins#index'
-  resources :pins do
-    
+  root to: 'boards#index'
+  resources :boards do
+    resources :pins
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
