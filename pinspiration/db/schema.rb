ac08adbad919b65a057626fefb9caa31c82b3505
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-13 11:14:01 -0500

    added new migrations, added user has many boards, pins are through boards

--- pinspiration/db/schema.rb
+++ pinspiration/db/schema.rb
@@ -11,18 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113142309) do
+ActiveRecord::Schema.define(version: 20151113160723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string  "name"
+    t.string  "description"
+    t.string  "photo_url"
+    t.integer "user_id"
+  end
+
+  add_index "boards", ["user_id"], name: "index_boards_on_user_id", using: :btree
 
   create_table "pins", force: :cascade do |t|
     t.string  "title"
     t.string  "photo_url"
     t.string  "description"
     t.integer "user_id"
+    t.integer "board_id"
   end
 
+  add_index "pins", ["board_id"], name: "index_pins_on_board_id", using: :btree
   add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +54,7 @@ ActiveRecord::Schema.define(version: 20151113142309) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "boards", "users"
+  add_foreign_key "pins", "boards"
   add_foreign_key "pins", "users"
 end
